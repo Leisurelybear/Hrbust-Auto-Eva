0f8b68116ddbe545f0e8ad3a957d0b6b04cb9bfd
jason <jasonzhang666@163.com>
2020-12-07 11:15:58 +0800

    ### v1.4: * 修复过多评价导致浏览器奔溃触发ipc保护的问题。 * 增加评价等级选择（好，中，差，随机），目前选择后只能手动提交每一次评价。 * 增加评语个性化设置，现在大家可以在代码开始位置设置自己想随机输入的评价内容。

--- Hrbust Auto-Eva.user.js
+++ Hrbust Auto-Eva.user.js
@@ -3,27 +3,63 @@
 // @require      https://cdn.staticfile.org/jquery/3.3.1/jquery.min.js
 // @name         哈尔滨理工大学 教务在线 教学评价、评估课程自动完成脚本 Hrbust Auto-Eva
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.4
 // @description  哈尔滨理工大学（hrbust） 教学评估自动完成脚本。在http://jwzx.hrbust.edu.cn/内，评估课程，教学评价自动完成脚本。使用方法：打开教务在线-点击"评估课程"/"教学评价"，稍等片刻，自动完成全部课程评价。
 // @author       Jason Zhang
+// @homepageURL  https://github.com/zhangxujie2018/Hrbust-Auto-Eva
+// @supportURL   https://github.com/zhangxujie2018/Hrbust-Auto-Eva/issues
 // @match        http://jwzx.hrbust.edu.cn/academic/eva/index/evaindexinfo.jsdo*
 // @match        http://jwzx.hrbust.edu.cn/academic/eva/index/resultlist.jsdo*
 // @run-at       document-end
+// @license      MIT
 // @icon         https://s3.ax1x.com/2020/11/22/DG9DVe.png
 // @icon64       https://s3.ax1x.com/2020/11/22/DG9DVe.png
 
 // ==/UserScript==
 (function () {
     'use strict';
-    // 设置 setting
-    var interval_time = 100; //设置模拟刷新时间，单位毫秒
-    var auto_commit = 1;//设置自动提交
-    //代码中randomApprove(), randomAdvice(), randomOther()方法可自定义评价内容
+/////////////////////////---- Settings -----///////////////////////////
+    var sleep_time = 1000; //设置模拟刷新时间，单位毫秒
+
+    var auto_commit = 1;//设置自动提交 1自动提交  0手动提交
+
+    var evaluation_level = 0;//设置评价等级，默认为0：关闭。其他参数： 1：完全好评，2：中等评价，3：较差评价，4：完全随机。注意：开启此选项，则需要手动提交每一次评价
+
+    //你认为教师在教学上最值得肯定之处？ 可按照格式拼接或删除建议
+    var text_approve = [
+        "活跃同学气氛 让同学们都能积极参于课堂问答",
+        "善待学生，师生融恰，学生成绩能稳定提高，学校、家长双满意即是值得肯定的优秀教师。",
+        "我觉得我们学校的老师教学水平是非常值得肯定的，学校里有很多年龄比较大的老师，他们都有特别丰富的人生阅历和渊博的知识，这些完美的履历是可以让我们敬畏他们的。",
+        "老师对学生的关心与帮助是无微不至的",
+    ];
+
+    //你认为教师在教学上最应该改进之处？可按照格式拼接或删除建议
+    var text_improve = [
+        "转变教育观念：传统教师与现代教师的区别应当有现代教育观，吸引新思想，树立新的教育观念。",
+        "拓展教学艺术，教学课堂要有创新，教师首先要努力实现教学观念的更新，以前的教学注重以教材为中心，以教测为目标。",
+        "很好",
+        "很不错",
+        "很完美",
+    ];
+
+    //你对本门课程选用教材及参考书有何意见和建议？ 可按照格式拼接或删除建议
+    var text_advice = [
+        "很好",
+        "教材应该具有实践性",
+        "教材应该具有人文性",
+        "教材应该具有综合性",
+        "教材应该具有思想性",
+    ];
+
+    //其他建议？可按照格式拼接或删除建议
+    var text_other = [
+        "很好",
+        "无",
+    ];
 
 
-    let interval = window.setInterval(listen, interval_time);
-
-    let count = 0;
+/////////////////////////---- start -----///////////////////////////
+    listen();
 
     function listen() {
         console.log("Auto-Eva Listening...");
@@ -33,39 +69,33 @@
         }
         if (window.location.href.indexOf("resultlist") !== -1) {
             let eva_tag = $("#li14 > a");
-
+            let count = 0;
             //let innerTabRow = $("body > center > table.infolist_tab > tbody > tr");
-            //console.log(innerTabRow)
-
-            //如果评估课程选中
-            //if (eva_tag.length !== 0 && eva_tag[0].className === "cur") {
-            console.log("--------")
-            // body > center > table.infolist_tab > tbody > tr:nth-child(8) > td:nth-child(4) > a
-            //body > table > tbody > tr:nth-child(2)
-            // let innerTabRow = $("body > center > table.infolist_tab > tbody > tr");
 
             //body > center > table.infolist_tab > tbody > tr:nth-child(1)
             let innerTabRow = $("body > center > table.infolist_tab > tbody > tr");
-
+            console.log(innerTabRow)
             for (let i = 1; i <= innerTabRow.length; i++) {
+                //console.log(innerTabRow[i])
                 //评估 a href标签  nth-child 选择第n个子节点，从1开始
                 let rowLink = $("body > center > table.infolist_tab > tbody > tr:nth-child(" + i + ") > td:nth-child(4) > a");
                 let evaStatus = $("body > center > table.infolist_tab > tbody > tr:nth-child(" + i + ") > td:nth-child(3)");
                 // body > center > table.infolist_tab > tbody > tr:nth-child(8) > td:nth-child(4) > a
-                if (typeof(evaStatus.html())!="undefined" && evaStatus.html().indexOf("未评估") !== -1 && rowLink.length !== 0) {
+                if (typeof (evaStatus.html()) != "undefined" && evaStatus.html().indexOf("未评估") !== -1 && rowLink.length !== 0) {
                     //未评估 可以点：评估
                     //console.log(rowLink[0].href)
-
                     window.parent.frames['mainFrame'].location.href = rowLink[0].href;
+                }
+                if (typeof (evaStatus.html()) != "undefined" && evaStatus.html().indexOf("已评估") !== -1) {
                     count++;
+                }
 
-                }
                 //body > table > tbody > tr:nth-child(8) > td:nth-child(4) > a
-                //body > table > tbody > tr:nth-child(8) > td:nth-child(4) > a
-                if (count >= innerTabRow.length) {//如果完成了，则取消这个监听器
-                    console.log("完成！！！")
-                    clearInterval(interval)
-                }
+            }
+            if (count >= innerTabRow.length - 1) {//如果完成了，则取消这个监听器
+                console.log("Finish all " + count + " item(s).")
+            } else {
+                console.log("Already finish " + count + " item(s), remain " + (innerTabRow.length - 1) + ". ")
             }
 
         }
@@ -76,23 +106,70 @@
     function eva_core() {
         //取出所有radios组件
         var radios = $("input:radio");
-        console.log(radios)
+        //console.log(radios)
         //有radios的行数
         let rowNums = radios.length % 4 === 0 ? radios.length / 4 : -1;
         console.log("radios评估共：" + rowNums + "行");
 
         if (rowNums !== -1) {
-            //除了最后一行总评，随机取出一个，评价为良好，其他全部优秀
             let level2 = Math.ceil(Math.random() * (rowNums - 1));
 
-            // console.log(rowNums)
-            for (let i = 0; i < rowNums; i++) {
-                if (i !== level2) {
-                    checkRadioIndexOf(radios, i, 0);
-                } else {
-                    checkRadioIndexOf(radios, i, 1);
-                }
+            //console.log("rowNums:" + rowNums)
+            switch (evaluation_level) {
+                case 0:
+                case 1: //优秀评价
+                    //除了最后一行总评，随机取出一个，评价为良好，其他全部优秀
+                    for (let i = 0; i < rowNums; i++) {
+                        if (i !== level2) {
+                            checkRadioIndexOf(radios, i, 0);
+                        } else {
+                            checkRadioIndexOf(radios, i, 1);
+                        }
+                    }
+                    break;
+                case 2: //中等评价
+                    for (let i = 0; i < rowNums; i++) {
+                        let rand0 = Math.ceil(Math.random() * (rowNums - 1));
+                        if (i !== level2) {
+                            checkRadioIndexOf(radios, i, ((rand0 & 1) + 1));
+                        } else {
+                            checkRadioIndexOf(radios, i, 0);
+                        }
+                    }
+                    break;
+                case 3: //较差评价
+                    for (let i = 0; i < rowNums; i++) {
+                        let rand0 = Math.ceil(Math.random() * (rowNums - 1));
+                        if (i !== level2) {
+                            checkRadioIndexOf(radios, i, ((rand0 & 1) + 2));
+                        } else {
+                            checkRadioIndexOf(radios, i, 0);
+                        }
+                    }
+                    break
+
+                case 4: //全随机评价
+                    //防止随机全重复，无法提交
+                    checkRadioIndexOf(radios, 0, 0);
+                    checkRadioIndexOf(radios, 1, 1);
+                    for (let i = 2; i < rowNums; i++) {
+                        let rand0 = Math.ceil(Math.random() * (rowNums - 1));
+                        checkRadioIndexOf(radios, i, (rand0 & 0b11));
+
+                    }
+                    break;
+                default: //默认优秀评价
+                    for (let i = 0; i < rowNums; i++) {
+                        if (i !== level2) {
+                            checkRadioIndexOf(radios, i, 0);
+                        } else {
+                            checkRadioIndexOf(radios, i, 1);
+                        }
+                    }
+                    break;
+
             }
+
 
         }
 
@@ -107,8 +184,23 @@
 
 
         //提交
-        if (auto_commit == 1) {
-            document.form1.submit();
+        if (auto_commit === 1 && evaluation_level === 0) {
+            //document.form1.sub_b.disabled=true;
+            var cn = 0;
+
+            for (let i = 0; i < document.form1.elements.length; i++) {
+                if (document.form1.elements[i].type === "radio") {
+                    if (document.form1.elements[i].checked) {
+                        cn++;
+                    }
+                }
+
+            }
+            //alert(cn)
+            setTimeout(function () {
+                document.form1.submit();
+            }, sleep_time);
+            //document.form1.submit();
         }
 
     }
@@ -122,12 +214,7 @@
     //你认为教师在教学上最值得肯定之处？ 随机一个 老师值得肯定的地方
     function randomApprove() {
         //str中可以自定义随机评价老师的内容
-        let str = [
-            "活跃同学气氛 让同学们都能积极参于课堂问答",
-            "善待学生，师生融恰，学生成绩能稳定提高，学校、家长双满意即是值得肯定的优秀教师。",
-            "我觉得我们学校的老师教学水平是非常值得肯定的，学校里有很多年龄比较大的老师，他们都有特别丰富的人生阅历和渊博的知识，这些完美的履历是可以让我们敬畏他们的。",
-            "老师对学生的关心与帮助是无微不至的",
-        ]
+        let str = text_approve;
 
 
         let rand = Math.ceil(Math.random() * str.length) - 1;
@@ -141,14 +228,7 @@
     //你认为教师在教学上最应该改进之处？ 随机一个 应该改进的地方
     function randomImprove() {
         //str中可以自定义随机还需提升的部分
-        let str = [
-            "转变教育观念：传统教师与现代教师的区别应当有现代教育观，吸引新思想，树立新的教育观念。",
-            "拓展教学艺术，教学课堂要有创新，教师首先要努力实现教学观念的更新，以前的教学注重以教材为中心，以教测为目标。",
-            "很好",
-            "很不错",
-            "很完美",
-        ];
-
+        let str = text_improve;
 
         let rand = Math.ceil(Math.random() * str.length) - 1;
         if (rand < 0) {
@@ -162,14 +242,7 @@
     //你对本门课程选用教材及参考书有何意见和建议？ 随机教材建议
     function randomAdvice() {
         //str中可以自定义随机还需提升的部分
-        let str = [
-            "很好",
-            "教材应该具有实践性",
-            "教材应该具有人文性",
-            "教材应该具有综合性",
-            "教材应该具有思想性",
-        ];
-
+        let str = text_advice
 
         let rand = Math.ceil(Math.random() * str.length) - 1;
         if (rand < 0) {
@@ -183,11 +256,7 @@
     //其他建议
     function randomOther() {
         //str中可以自定义随机还需提升的部分
-        let str = [
-            "很好",
-            "无",
-        ];
-
+        let str = text_other;
 
         let rand = Math.ceil(Math.random() * str.length) - 1;
         if (rand < 0) {
